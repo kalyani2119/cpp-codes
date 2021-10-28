14f4796c13fa5208946f12c2cea535fd23354757
KALYANI S <91209741+kalyani2119@users.noreply.github.com>
2021-10-28 22:40:31 +0530

    Create ciphering.cpp

---
+++ ciphering.cpp
@@ -0,0 +1,12 @@
+//Ciphering the letters
+#include<iostream>
+using namespace std;
+int main()
+{
+  char a,b,c,d,e,f;
+  cout<<"Enter your code"<<endl;
+  cin>>a>>b>>c>>d>>e>>f;
+  cout<<"Your hidden code is : "<<int(a)<<int(b)<<int(c)<<int(d)<<int(e)<<int(f);
+  system("pause>0");
+}
+
