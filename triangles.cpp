38007c612595b329cb33e4a8a8f7bf353211fbe8
KALYANI S <91209741+kalyani2119@users.noreply.github.com>
2021-10-28 22:49:52 +0530

    Update triangles.cpp

--- triangles.cpp
+++ triangles.cpp
@@ -1,1 +1,24 @@
+//To check whether the triangles are equilateral triangles,isosceles or scalene triangle
+#include <iostream>
+using namespace std;
+int main()
+{
+  int x,y,z;
+  cout<<"Enter the sides of the triangle"<<endl;
+  cin>>x>>y>>z;
+  if(x==y&&y==z&&z==x)
+  {
+    cout<<"It's an equilateral triangle";
+  }
+  else
+  {
+    if(x!=y&&y!=z&&z!=x)
+      cout<<"It's a scalene triangle";
+    else
+      cout<<"It's an isosceles triangle";
+  }
+  system("pause>0");
+}
 
+      
+  
