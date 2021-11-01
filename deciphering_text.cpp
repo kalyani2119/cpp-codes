35daeec59d0afae7dac86cedc2c7def8b7d121f0
KALYANI S <91209741+kalyani2119@users.noreply.github.com>
2021-11-01 13:49:57 +0530

    Update deciphering_text.cpp

--- deciphering_text.cpp
+++ deciphering_text.cpp
@@ -1,1 +1,10 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,b,c,d,e;
+  cout<<"Enter the numbers";
+  cin>>a>>b>>c>>d>>e;
+  cout<<"hidden message"<<char(a)<<char(b)<<char(c)<<char(d)<<char(e);
+  system("pause>0");
+}
