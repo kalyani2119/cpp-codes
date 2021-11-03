bfb1d6a6ceb9291b223f2be90bd04aed168b4f51
KALYANI S <91209741+kalyani2119@users.noreply.github.com>
2021-11-03 15:08:08 +0530

    Update divisibility.cpp

--- divisibility.cpp
+++ divisibility.cpp
@@ -1,1 +1,14 @@
-
+/* To check the numbers that are divisible by 2 and 4 between 10-100 */
+#include<iostream>
+using namespace std;
+int main()
+{
+  int counter = 10;
+  while(counter<=100)
+  {
+    if(counter%2==0 && counter%4==0)
+      cout<<"These are the numbers which are divisible by 2 and 4"<<counter;
+   counter++;
+  }
+  system("pause>0");
+}
