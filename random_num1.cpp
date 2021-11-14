55b3495e3dbd4a30e089c97b7a6b1915af94b2e8
KALYANI S <91209741+kalyani2119@users.noreply.github.com>
2021-11-14 23:25:40 +0530

    Add files via upload

---
+++ random_num1.cpp
@@ -0,0 +1,14 @@
+//To generate ten Random numbers using the function rand()
+#include<iostream>
+using namespace std;
+#include<cstdlib>
+int main()
+{
+	int num,i;
+	for(i=0;i<10;i++)
+	{
+		cout<<"The random numbers are:"<<(num=rand())<<endl;
+		
+	}
+	system("pause>0");
+}
