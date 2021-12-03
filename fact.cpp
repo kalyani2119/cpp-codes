1d98697c556217f6a893f865177cb4895809c70d
KALYANI S <91209741+kalyani2119@users.noreply.github.com>
2021-12-03 21:28:16 +0530

    Update fact.cpp

--- fact.cpp
+++ fact.cpp
@@ -1,1 +1,18 @@
+#include<iostream.h>
+#include<stdlib.h>
+int main()
+{
+  system("cls");
+  int i,num,fact=1;
+  cout<<"Please enter the number";
+  cin>>num;
+  i=num;
+  while(num)
+  {
+    fact=fact*num;
+    --num;
+  }
+  cout<<"The factorial of the number:"<<i<<"is"<<fact<<"\n";
+  return 0;
+}
 
